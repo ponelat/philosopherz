7315f1fab6792a5e543134d0476292321ac34fa4
Josh Ponelat <josh@ponelat.com>
2015-03-26 13:39:54 +0200

    add bs script

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ var less = require('gulp-less');
 
 gulp.task('default', ['less']);
 
+// Watch less files
+gulp.task('watch', function() {
+  gulp.watch('less/*.less', ['less']);
+})
+
 // Less compiling
 gulp.task('less', function() {
   return gulp.src('less/*.less')
